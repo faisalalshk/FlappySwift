5be917a53e6b6c7ab9a5bb20efc005cf1ef5afb9
faisalalshk <faisalalshk1@gmail.com>
2025-05-17 16:47:58 +0300

    Update project to SwiftUI

--- FlappyBirdTests/FlappyBirdTests.swift
+++ FlappyBirdTests/FlappyBirdTests.swift
@@ -22,12 +22,12 @@ class FlappyBirdTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        XCTAssertTrue(true)
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
